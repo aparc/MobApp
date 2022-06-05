23215ef7db8bb38d9ae33e045209a00147c49b63
Andrei <andrei.parchukov@t-systems.com>
2022-06-06 02:08:14 +0300

    Initial Commit

---
+++ MobApp/MobAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobAppApp.swift
+//  MobApp
+//
+//  Created by Андрей on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MobAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
